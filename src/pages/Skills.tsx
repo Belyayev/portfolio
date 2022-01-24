c7097918ad36afa2d70a48970e6149c356ac7863
Mike <4xgood@gmail.com>
2022-01-24 15:08:02 -0700

    added most skills to the view

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -2,12 +2,27 @@ import SVGDialCircle from "../components/SvgDialCircle";
 
 function Skills() {
   return (
-    <div className="skills">
-      <SVGDialCircle color="rgb(45, 213, 255)" />
-      <SVGDialCircle color="rgb(45, 213, 255)" />
-      <SVGDialCircle color="rgb(45, 213, 255)" />
-      <SVGDialCircle color="rgb(45, 213, 255)" />
-    </div>
+    <>
+      <h2>My skills level at different technologies:</h2>
+      <div className="skills">
+        <SVGDialCircle color="rgb(45, 213, 255)" text="HTML" />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="CSS" />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="JavaScript" />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="React" />
+      </div>
+      <div className="skills">
+        <SVGDialCircle color="rgb(45, 213, 255)" text="C#" />
+        <SVGDialCircle color="rgb(45, 213, 255)" text=".Net" />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="SQL" />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="Entity FW" />
+      </div>
+      <div className="skills">
+        <SVGDialCircle color="rgb(45, 213, 255)" text="Azure" />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="Python" />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="Angular" />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="TypeScript" />
+      </div>
+    </>
   );
 }
 
