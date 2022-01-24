8a5d4f82499400d5951dbb3401ecf8b183b45ed3
Mike <4xgood@gmail.com>
2022-01-24 15:53:42 -0700

    added hover fade on skill cards

--- src/components/SvgDialCircle.tsx
+++ src/components/SvgDialCircle.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 interface ISvg {
   x?: number;
   y?: number;
@@ -6,10 +8,30 @@ interface ISvg {
 }
 
 function SVGDialCircle(props: ISvg) {
+  const [hover, setHover] = useState(false);
+
   let color = "lime";
   if (props.color) {
     color = props.color;
   }
+
+  let cardStyle;
+  if (hover) {
+    cardStyle = {
+      transition: "1s ease",
+      fill: "black",
+      fillOpacity: "0.1",
+      stroke: color,
+    };
+  } else {
+    cardStyle = {
+      transition: "1s ease",
+      fill: color,
+      fillOpacity: "0.2",
+      stroke: color,
+    };
+  }
+
   let y = 105;
   if (props.y) y = props.y;
   let text = props.text;
@@ -19,12 +41,13 @@ function SVGDialCircle(props: ISvg) {
       style={{
         margin: "1rem",
       }}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
     >
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 97 110" width="100%">
         <g stroke="none" fill={color}>
           <polygon
-            fillOpacity={0.15}
-            stroke={color}
+            style={cardStyle}
             points="1,0 32,0 40,5 88,5 91,1 97,1 97,88 84,102 62,102 60,94 1,94 1,81 3,76 3,41 1,37 "
           />
           <polygon points="98,90 98,101 87,101 " />
