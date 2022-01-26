b2aa5c108ec63e03a6cee1cb46813e0b1268e8e1
Mike <4xgood@gmail.com>
2022-01-25 20:19:18 -0700

    skill card is done

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -5,18 +5,22 @@ function Skills() {
     <>
       <h2>My skills level at different technologies:</h2>
       <div className="skills">
-        <SVGDialCircle color="rgb(45, 213, 255)" text="HTML" value={90} />
-        <SVGDialCircle color="rgb(45, 213, 255)" text="CSS" value={95} />
-        <SVGDialCircle color="rgb(45, 213, 255)" text="JavaScript" value={70} />
-        <SVGDialCircle color="rgb(45, 213, 255)" text="React" />
-        <SVGDialCircle color="rgb(45, 213, 255)" text="C#" />
-        <SVGDialCircle color="rgb(45, 213, 255)" text=".Net" />
-        <SVGDialCircle color="rgb(45, 213, 255)" text="SQL" />
-        <SVGDialCircle color="rgb(45, 213, 255)" text="Entity Frmwrk" />
-        <SVGDialCircle color="rgb(45, 213, 255)" text="Azure" />
-        <SVGDialCircle color="rgb(45, 213, 255)" text="Python" />
-        <SVGDialCircle color="rgb(45, 213, 255)" text="Angular" />
-        <SVGDialCircle color="rgb(45, 213, 255)" text="TypeScript" />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="HTML" value={94} />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="CSS" value={92} />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="JavaScript" value={77} />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="React" value={80} />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="C#" value={65} />
+        <SVGDialCircle color="rgb(45, 213, 255)" text=".Net" value={60} />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="SQL" value={70} />
+        <SVGDialCircle
+          color="rgb(45, 213, 255)"
+          text="Entity Frmwrk"
+          value={80}
+        />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="Azure" value={68} />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="Python" value={72} />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="Angular" value={56} />
+        <SVGDialCircle color="rgb(45, 213, 255)" text="TypeScript" value={60} />
       </div>
     </>
   );
