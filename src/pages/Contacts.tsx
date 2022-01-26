951b95c41ed55701487dc9e66aa83438c0a88a36
Belyayev <4xgood@gmail.com>
2022-01-26 08:42:35 -0700

    updated title

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -2,6 +2,15 @@ function Contacts() {
   return (
     <div>
       <h1>Contacts page</h1>
+      <h3>
+        <span>GitHub:</span>https://github.com/belyayev
+      </h3>
+      <h3>
+        <span>Linked-in:</span>https://github.com/belyayev
+      </h3>
+      <h3>
+        <span>e-mail:</span>4xgood@gmail.com
+      </h3>
     </div>
   );
 }
