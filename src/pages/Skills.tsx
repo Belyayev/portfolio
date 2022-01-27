0156faf019ac5ed4ac0e0dea60aefce53d20b020
Belyayev <4xgood@gmail.com>
2022-01-27 11:56:41 -0700

    overflow fix

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -6,22 +6,32 @@ function Skills() {
       <h2>My skills level at different technologies:</h2>
       <div className="skills">
         <SVGDialCircle color="#2dd5ff" text="HTML" value={94} />
-        <SVGDialCircle color="#2dd5ff" text="CSS" value={92} delay={1000} />
+        <SVGDialCircle color="#2dd5ff" text="CSS" value={92} delay={100} />
         <SVGDialCircle
           color="#2dd5ff"
           text="JavaScript"
           value={77}
-          delay={2000}
+          delay={200}
         />
-        <SVGDialCircle color="#2dd5ff" text="React" value={80} />
-        <SVGDialCircle color="#2dd5ff" text="C#" value={65} />
-        <SVGDialCircle color="#2dd5ff" text=".Net" value={60} />
-        <SVGDialCircle color="#2dd5ff" text="SQL" value={70} />
-        <SVGDialCircle color="#2dd5ff" text="EntityFrmwrk" value={80} />
-        <SVGDialCircle color="#2dd5ff" text="Azure" value={68} />
-        <SVGDialCircle color="#2dd5ff" text="Python" value={72} />
-        <SVGDialCircle color="#2dd5ff" text="Angular" value={56} />
-        <SVGDialCircle color="#2dd5ff" text="TypeScript" value={60} />
+        <SVGDialCircle color="#2dd5ff" text="React" value={80} delay={300} />
+        <SVGDialCircle color="#2dd5ff" text="C#" value={65} delay={400} />
+        <SVGDialCircle color="#2dd5ff" text=".Net" value={60} delay={500} />
+        <SVGDialCircle color="#2dd5ff" text="SQL" value={70} delay={600} />
+        <SVGDialCircle
+          color="#2dd5ff"
+          text="EntityFrmwrk"
+          value={80}
+          delay={700}
+        />
+        <SVGDialCircle color="#2dd5ff" text="Azure" value={68} delay={800} />
+        <SVGDialCircle color="#2dd5ff" text="Python" value={72} delay={900} />
+        <SVGDialCircle color="#2dd5ff" text="Angular" value={56} delay={1000} />
+        <SVGDialCircle
+          color="#2dd5ff"
+          text="TypeScript"
+          value={60}
+          delay={1100}
+        />
       </div>
     </>
   );
