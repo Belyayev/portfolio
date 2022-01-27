6c9a4bd2dc16b56462e8178a7d2e84a23737cbe7
Mike <4xgood@gmail.com>
2022-01-27 15:15:22 -0700

    updated skill values

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -5,12 +5,12 @@ function Skills() {
     <>
       <h2>My skills level at different technologies:</h2>
       <div className="skills">
-        <SVGDialCircle color="#2dd5ff" text="HTML" value={94} delay={100} />
-        <SVGDialCircle color="#2dd5ff" text="CSS" value={92} delay={200} />
+        <SVGDialCircle color="#2dd5ff" text="HTML" value={95} delay={100} />
+        <SVGDialCircle color="#2dd5ff" text="CSS" value={95} delay={200} />
         <SVGDialCircle
           color="#2dd5ff"
           text="JavaScript"
-          value={77}
+          value={75}
           delay={300}
         />
         <SVGDialCircle color="#2dd5ff" text="React" value={80} delay={400} />
@@ -20,12 +20,12 @@ function Skills() {
         <SVGDialCircle
           color="#2dd5ff"
           text="EntityFrmwrk"
-          value={80}
+          value={60}
           delay={800}
         />
-        <SVGDialCircle color="#2dd5ff" text="Azure" value={68} delay={900} />
-        <SVGDialCircle color="#2dd5ff" text="Python" value={72} delay={1000} />
-        <SVGDialCircle color="#2dd5ff" text="Angular" value={56} delay={1100} />
+        <SVGDialCircle color="#2dd5ff" text="Azure" value={65} delay={900} />
+        <SVGDialCircle color="#2dd5ff" text="Python" value={40} delay={1000} />
+        <SVGDialCircle color="#2dd5ff" text="Angular" value={45} delay={1100} />
         <SVGDialCircle
           color="#2dd5ff"
           text="TypeScript"
