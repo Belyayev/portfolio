6d3fbbf4097825e53075dc8414f66306eaf6044b
Mike Belyayev <4xgood@gmail.com>
2024-10-18 18:24:20 -0600

    added resume:

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,4 +1,28 @@
-import { CSSProperties } from "react";
+import { CSSProperties, useState } from "react";
+import "../Resume.css";
+
+const Section = ({ jobTitle, workPlace, date, details }: any) => {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <div className="section">
+      <div className="section-header" onClick={() => setExpanded(!expanded)}>
+        <div>
+          <span className="job-title">{jobTitle},</span>
+          <span className="work-place">{workPlace}</span>
+        </div>
+        <div className="date">{date}</div>
+      </div>
+      {expanded && (
+        <ul className="section-details">
+          {details.map((detail: any, index: any) => (
+            <li key={index}>{detail}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 function Resume() {
   return (
@@ -7,6 +31,122 @@ function Resume() {
         <span style={{ "--index": 0 } as CSSProperties}>Resume</span>
         <span style={{ "--index": 1 } as CSSProperties}>Resume</span>
         <span style={{ "--index": 2 } as CSSProperties}>Resume</span>
+      </div>
+      <div className="resume">
+        <div className="block">
+          <h1>
+            <span className="textLabel">Objective:</span>
+            <span className="textRegular">
+              Software Developer, Data Engineer
+            </span>
+          </h1>
+        </div>
+        <div className="block">
+          <h1>
+            <span className="textLabel">Summary of Qualifications:</span>
+          </h1>
+          <ul>
+            <li>
+              Over 4 years of comprehensive experience in full-stack web
+              application development, proficient in React, Node, and SQL.
+            </li>
+            <li>
+              Skilled data engineer with extensive experience in ETL processes,
+              data analysis, and database management, particularly in Azure
+              environments.
+            </li>
+            <li>
+              Proven track record as an IT professional with 20 years of
+              experience in various roles, including network engineering, IT
+              project management, and server/desktop support.
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h1>
+            <span className="textLabel">Professional Experience:</span>
+          </h1>
+        </div>
+        <Section
+          jobTitle="Software Developer"
+          workPlace="ExxonMobil Canada / Imperial, Calgary, AB"
+          date="Oct 2020 – present"
+          details={[
+            "Development of the Web hosted applications for the company such as time management and work permitting, well monitoring and maintenance, value tracking of the field processes.",
+            "Heavy front end focus that includes UI/UX user research, design and implementation.",
+            "Back end development using node, C#, SQL, Cosmos DB, Azure DevOps tools.",
+            "Working in as small squad 5-7 developers following Agile framework, Scrum.",
+          ]}
+        />
+        <Section
+          jobTitle="Network Analyst"
+          workPlace="ExxonMobil Canada, Calgary, AB"
+          date="Sep 2015 – Oct 2020"
+          details={[
+            "Supported large corporate network built on Cisco and Juniper devices.",
+            "Day to day monitoring and troubleshooting, configuration updates and reporting.",
+            "End of life device replacement (busy core and distribution network switches and smaller for access layer).",
+            "Supported wireless network system “Rajant” with over 200 mobile units in the mine and 20 nodes.",
+          ]}
+        />
+        <Section
+          jobTitle="IT Projects Analyst"
+          workPlace="ExxonMobil Canada, Calgary, AB"
+          date="Sep 2010 – Oct 2015"
+          details={[
+            "Worked on many projects across Canada, US and Africa to implement IT infrastructure on the drilling rigs. Offshore and land. L4 business networks and L3 process control.",
+            "Designed, deployed and maintained various networks including fiber, wireless, satellite and cellular technology.",
+            "Configured switching and routing on Cisco, Juniper and other vendors.",
+            "Project coordination with heavy hands on component to deliver infrastructure in challenging environments (offshore, remote sites, large and small offices).",
+          ]}
+        />
+        <Section
+          jobTitle="Technical Analyst – Network"
+          workPlace="AEGON Canada, Toronto, ON"
+          date="Apr 2010 – Sep 2010"
+          details={[
+            "Supported network infrastructure and Cisco IP / Nortel PBX telephony at company data center.",
+            "Virtual servers support built on VMware and Hyper-V.",
+          ]}
+        />
+
+        <Section
+          jobTitle="IT Infrastructure Analyst"
+          workPlace="ExxonMobil, Atyrau, Kazakhstan"
+          date="Apr 2008 – Feb 2010"
+          details={[
+            "Provided level-2 onsite IT support for 2 main upstream offices and remote drilling sites.",
+            "Supported IT infrastructure: hardware and software installation, backup media handling, vendors and telecom providers coordination.",
+            "Developed IT manuals and procedures, provided orientation training for new users.",
+          ]}
+        />
+
+        <Section
+          jobTitle="IT Service Desk"
+          workPlace="Agip KCO, Kazakhstan"
+          date="Aug 2007 – Mar 2008"
+          details={[
+            "User support (remote and desk side).",
+            "Supported Win 2003, AD and MS Exchange servers and SAP application.",
+            "Installation and maintenance of workstations and a variety of network equipment, printers, MFP devices, and scanners.",
+          ]}
+        />
+
+        <Section
+          jobTitle="IT & Telecom Infrastructure Analyst"
+          workPlace="Karachaganak Petroleum"
+          date="Jan 2007 – Jul 2007"
+          details={[
+            "Technical onsite support of computer systems and telecommunications on the pipeline terminal, including: VSAT, SDH, DWDM, hotlines, trunk radio, PBX, network switches, and routers.",
+          ]}
+        />
+        <div className="block">
+          <h1>
+            <span className="textLabel">Education:</span>
+          </h1>
+          <div>Bachelor of Science, in Mechanical Engineering</div>
+          <div>1999 – 2004 West Kazakhstan Technical University</div>
+        </div>
       </div>
     </div>
   );
